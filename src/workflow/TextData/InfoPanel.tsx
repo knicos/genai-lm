3f2aa9ab01af486ae9434a788de75978fc3cf666
Nicolas Pope <nicolas.pope@uef.fi>
2025-12-09 11:58:13 +0000

    Merged PR 1032: Small restyle of text data info
    Small restyle of text data info
    
    Related work items: #1500

--- src/workflow/TextData/InfoPanel.tsx
+++ src/workflow/TextData/InfoPanel.tsx
@@ -1,7 +1,7 @@
-import { Alert } from '@mui/material';
 import style from './InfoPanel.module.css';
 import { Button } from '@genai-fi/base';
 import { useTranslation } from 'react-i18next';
+import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
 
 interface Props {
     show?: boolean;
@@ -10,28 +10,25 @@ interface Props {
     message: string;
 }
 
-export default function InfoPanel({ show, onClose, severity, message }: Props) {
+export default function InfoPanel({ show, onClose, message }: Props) {
     const { t } = useTranslation();
     if (!show) return null;
 
     return show ? (
-        <div className={style.container}>
-            <div className={style.innerContainer}>
-                <Alert
-                    severity={severity}
-                    className={style.alert}
+        <div className={style.innerContainer}>
+            <InfoOutlineIcon
+                fontSize="large"
+                color="inherit"
+            />
+            {message}
+            {onClose && (
+                <Button
+                    onClick={onClose}
+                    variant="contained"
                 >
-                    {message}
-                </Alert>
-                {onClose && (
-                    <Button
-                        onClick={onClose}
-                        variant="contained"
-                    >
-                        {t('data.ok')}
-                    </Button>
-                )}
-            </div>
+                    {t('data.ok')}
+                </Button>
+            )}
         </div>
     ) : null;
 }
